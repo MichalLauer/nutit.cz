a7f19ab1e6c956c41924f7fba3a201e3d58b424e
Michal Lauer <michal.lauer.25@gmail.com>
2023-04-29 03:35:41 +0200

    Deleted redundant files; finalized for second send-offL

--- R/read_file.R
+++ R/read_file.R
@@ -2,12 +2,15 @@ read_file <- function(file, row) {
   # Personal restrictions
   nm <- 100
   base_skip <- 1000000
+  # Read file
   raw_data <- vroom::vroom(file = file, delim = "|", col_names = F,
                            skip = base_skip, n_max = nm, comment = "#",
                            show_col_types = F)
+
+  # Subset for specific row
   data <- subset(raw_data, X1 == row)
 
-  # Get column names
+  # Rename data set
   cols <- get_names(row)
   colnames(data) <- cols
 
--- main.R
+++ main.R
@@ -1,22 +1,17 @@
 # Read functions
 sapply(list.files("R", full.names = T), source)
+library(tictoc)
 # Prepare Parallel Processing
 cores <- future::availableCores() - 1
 future::plan(future::multicore, workers = cores)
-
+# Get data
 x <- timeseries(NULL, from = "2020-09-01", to = "2020-09-04", row = "Q")
+# Save
 x |>
-  write.csv(file = "out2.csv")
+  write.csv(file = "out2.csv", row.names = F)
 
 # ------------------------------------------------------------------------------
-library(profvis)
-library(tictoc)
-sapply(list.files("R", full.names = T), source)
-cores <- future::availableCores() - 1
-future::plan(future::multicore, workers = cores)
-tic()
-profvis({
-  x <- timeseries(NULL, from = "2020-09-01", to = "2020-09-04", row = "Q")
-})
-toc()
+
+
+
 
