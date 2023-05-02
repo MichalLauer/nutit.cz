22bd2b1039a2ebcde904e68c075cef7f3e54a45c
Michal Lauer <michal.lauer.25@gmail.com>
2023-05-02 21:04:08 +0200

    Added comments

--- R/get_names.R
+++ R/get_names.R
@@ -1,27 +1,29 @@
 get_names <- function(row) {
   def <- list(
+    # Transaction
     "Q" = c("<TYPE>","<TAS.SEQ>","<RNR.END.EXCH.SEQ>","<ACTIVITY.DATETIME>",
             "<BID.PRICE>","<BID.SIZE>","<ASK.PRICE>","<ASK.SIZE>",
             "<QUOTE.COND_1>","<PART.CODE>","<QUOTE.DATETIME>",
             "<EXCH.MESSAGE.TIMESTAMP>","<IMPLIED.BID.SIZE>",
             "<IMPLIED.ASK.SIZE>","<RFQ.SIZE>","<RFQ.SIDE>","<END.UPDATE.FLAG>"),
-    "H" = c("<ENUM.SRC.ID>","<SYMBOL.TICKER>","<CURRENCY.STRING>","<ISIN>",
-            "<SEDOL>","<CUSIP>","<DISPLAY.PRECISION>","<TRADE.OPEN>",
+    # Ticker identification
+    "H" = c("<TYPE>","<ENUM.SRC.ID>","<SYMBOL.TICKER>","<CURRENCY.STRING>",
+            "<ISIN>", "<SEDOL>","<CUSIP>","<DISPLAY.PRECISION>","<TRADE.OPEN>",
             "<OPEN_INT>","<YEST.TRADE.CLOSE>","<YEST.TRADE.VOL>",
             "<SETTLE.DATE>","<SETTLE.PRICE>","<EXPIRATION.DATE>",
             "<FRONT.MONTH.CONTRACT>","<ENUM.INSTR.TYPE>","<LOT.SIZE>",
             "<CONTRACT.SIZE>","<VARIABLE.TICK.SIZE>","<MKT.SEGMENT>",
             "<SETTLE.IND>","<SETTLEMENT.CURRENCY>"),
-    "T" = c("<TAS.SEQ>","<RNR.END.EXCH.SEQ>","<ACTIVITY.DATETIME>",
+    "T" = c("<TYPE>","<TAS.SEQ>","<RNR.END.EXCH.SEQ>","<ACTIVITY.DATETIME>",
             "<TRADE.PRICE>","<TRADE.SIZE>","<TRADE.COND_1>","<PART.CODE>",
             "<VWAP>","<TRADE.DATETIME>","<EXCH.MESSAGE.TIMESTAMP>",
             "<TRADE.COND_2>","<TRADE.COND_3>","<TRADE.OFFICIAL.TIME>",
             "<TRADE.COND_4>","<TRADE.COND_5>","<EXTENDED.TRADE.COND>",
             "<TRADE.OFFICIAL.DATE>","<RETRANSMISSION.FLAG>"),
-    "B" = c("<TAS.SEQ>","<RNR.END.EXCH.SEQ>","<ACTIVITY.DATETIME>",
+    "B" = c("<TYPE>","<TAS.SEQ>","<RNR.END.EXCH.SEQ>","<ACTIVITY.DATETIME>",
             "<BID.PRICE>","<BID.SIZE>","<BID.PART.CODE>","<ASK.PRICE>",
             "<ASK.SIZE>","<ASK.PART.CODE>","<EXCH.MESSAGE.TIMESTAMP>"),
-    "S" = c("<TAS.SEQ>","<ACTIVITY.DATETIME>","<TRADABLE.STATUS>",
+    "S" = c("<TYPE>","<TAS.SEQ>","<ACTIVITY.DATETIME>","<TRADABLE.STATUS>",
             "<MARKET.PHASE>","<EXCH.MESSAGE.TIMESTAMP>","<STATUS.START.DATE>")
   )
 
