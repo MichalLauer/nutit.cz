a62fc6f8a829b275608acde14d0d36c0a855bdf4
Michal Lauer <michal.lauer.25@gmail.com>
2023-05-02 20:41:47 +0200

    Added all possible rows

--- R/get_names.R
+++ R/get_names.R
@@ -4,7 +4,25 @@ get_names <- function(row) {
             "<BID.PRICE>","<BID.SIZE>","<ASK.PRICE>","<ASK.SIZE>",
             "<QUOTE.COND_1>","<PART.CODE>","<QUOTE.DATETIME>",
             "<EXCH.MESSAGE.TIMESTAMP>","<IMPLIED.BID.SIZE>",
-            "<IMPLIED.ASK.SIZE>","<RFQ.SIZE>","<RFQ.SIDE>","<END.UPDATE.FLAG>")
+            "<IMPLIED.ASK.SIZE>","<RFQ.SIZE>","<RFQ.SIDE>","<END.UPDATE.FLAG>"),
+    "H" = c("<ENUM.SRC.ID>","<SYMBOL.TICKER>","<CURRENCY.STRING>","<ISIN>",
+            "<SEDOL>","<CUSIP>","<DISPLAY.PRECISION>","<TRADE.OPEN>",
+            "<OPEN_INT>","<YEST.TRADE.CLOSE>","<YEST.TRADE.VOL>",
+            "<SETTLE.DATE>","<SETTLE.PRICE>","<EXPIRATION.DATE>",
+            "<FRONT.MONTH.CONTRACT>","<ENUM.INSTR.TYPE>","<LOT.SIZE>",
+            "<CONTRACT.SIZE>","<VARIABLE.TICK.SIZE>","<MKT.SEGMENT>",
+            "<SETTLE.IND>","<SETTLEMENT.CURRENCY>"),
+    "T" = c("<TAS.SEQ>","<RNR.END.EXCH.SEQ>","<ACTIVITY.DATETIME>",
+            "<TRADE.PRICE>","<TRADE.SIZE>","<TRADE.COND_1>","<PART.CODE>",
+            "<VWAP>","<TRADE.DATETIME>","<EXCH.MESSAGE.TIMESTAMP>",
+            "<TRADE.COND_2>","<TRADE.COND_3>","<TRADE.OFFICIAL.TIME>",
+            "<TRADE.COND_4>","<TRADE.COND_5>","<EXTENDED.TRADE.COND>",
+            "<TRADE.OFFICIAL.DATE>","<RETRANSMISSION.FLAG>"),
+    "B" = c("<TAS.SEQ>","<RNR.END.EXCH.SEQ>","<ACTIVITY.DATETIME>",
+            "<BID.PRICE>","<BID.SIZE>","<BID.PART.CODE>","<ASK.PRICE>",
+            "<ASK.SIZE>","<ASK.PART.CODE>","<EXCH.MESSAGE.TIMESTAMP>"),
+    "S" = c("<TAS.SEQ>","<ACTIVITY.DATETIME>","<TRADABLE.STATUS>",
+            "<MARKET.PHASE>","<EXCH.MESSAGE.TIMESTAMP>","<STATUS.START.DATE>")
   )
 
   r <- def[[row]]
@@ -13,8 +31,7 @@ get_names <- function(row) {
 }
 
 
-# "<TYPE>|<TAS.SEQ>|<RNR.END.EXCH.SEQ>|<ACTIVITY.DATETIME>|<BID.PRICE>|<BID.SIZE>|<ASK.PRICE>|<ASK.SIZE>|<QUOTE.COND_1>|
-# <PART.CODE>|<QUOTE.DATETIME>|<EXCH.MESSAGE.TIMESTAMP>|<IMPLIED.BID.SIZE>|<IMPLIED.ASK.SIZE>|<RFQ.SIZE>|<RFQ.SIDE>|<END.UPDATE.FLAG>" |>
+# "<TAS.SEQ>|<ACTIVITY.DATETIME>|<TRADABLE.STATUS>|<MARKET.PHASE>|<EXCH.MESSAGE.TIMESTAMP>|<STATUS.START.DATE>" |>
 #   strsplit("|", fixed = T) |>
 #   {\(x) x[[1]]}() |>
 #   paste(collapse = r'(",")')
