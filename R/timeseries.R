b53430d0eea404495c49662e4f0508c2de0ad032
Michal Lauer <michal.lauer.25@gmail.com>
2023-05-02 21:04:46 +0200

    Added check if clean function exists

--- R/timeseries.R
+++ R/timeseries.R
@@ -10,7 +10,13 @@ timeseries <- function(ticker, from, to = from, row) {
     purrr::list_rbind()
 
   # Clean the data based on the row defined
+  # If function does not exists, return raw data
   fx <- paste0("clean_", tolower(row))
+  if (!exists(fx)) {
+    warning(glue::glue("Function {fx} is not defined.\n",
+                       "  Returing a raw data set for function creation."))
+    return(data)
+  }
   data_clean <- do.call(fx, list(data = data))
 
   return(data_clean)
